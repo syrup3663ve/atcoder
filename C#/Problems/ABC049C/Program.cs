72942a973493917f0c9a37f5b660c0d9c44cece6
syrup3663ve <72807083+syrup3663ve@users.noreply.github.com>
2022-06-14 20:25:13 +0900

    ABC049C

---
+++ C#/Problems/ABC049C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        var s = Console.ReadLine();
+
+        s = s.Replace("eraser", "").Replace("erase", "").Replace("dreamer", "").Replace("dream", "");
+        
+        var result = s.Length == 0 ? "YES" : "NO";
+        Console.WriteLine(result);
+    }
+}
